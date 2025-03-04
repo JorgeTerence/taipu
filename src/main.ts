7307a2634d1561e0242d766113134d0d75e0a243
Jorge Terence <jorge.terence@outlook.com>
2025-03-04 20:06:52 -0300

    npm run build big sad

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import "./style.css";
 import {setupAlphabetPicker, setupTranscriber} from "./transcriber.ts";
 
 var alphabet = {};
+console.debug(alphabet); // just so that npm run build won't complain
 
 setupAlphabetPicker(document.querySelector<HTMLInputElement>("#kana")!, document.querySelector<HTMLInputElement>("#from")!);
 setupTranscriber(document.querySelector<HTMLInputElement>("#from")!, document.querySelector<HTMLParagraphElement>("#to")!);
